72d9ad04dbd78e25b73d38cb0845ec24c27f0df4
LaRaHh <45185758+LaRaHh@users.noreply.github.com>
2020-12-21 11:16:58 -0300

    Estilo do componente Tarefa

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -23,5 +23,31 @@ export default {
 </script>
 
 <style>
+    .tarefa{
+        box-sizing: border-box;
+        width: 350px;
+        height: 150px;
+        padding: 10px;
+        border-radius: 8px;
+        font-size: 2rem;
+        font-weight: 300;
+        cursor: pointer;
+        user-select:none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 5px;
+    }
 
+    .pendente {
+        border-left: 12px solid #B73229;
+        background-color: #F44336;
+    }
+
+    .feito{
+        color: #DDD;
+        border-left: 12px solid #0A8f08;
+        background-color: #4CAF50;
+        text-decoration: line-through;
+    }
 </style>
